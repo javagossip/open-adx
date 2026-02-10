a7427f2e26771552cd1d6fcb71f8e7120b673a4d
javagossip <javagossip@gmail.com>
2026-02-11 01:02:17 +0800

    feat: 为site_ad_placement表添加唯一索引uniq_idx_sadp_code

--- open-adexchange-mos/src/main/scripts/sql/alter_table_20260210.sql
+++ open-adexchange-mos/src/main/scripts/sql/alter_table_20260210.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `ad_slot_stat` ADD `ad_slot_name` VARCHAR(100)  NULL  DEFAULT NULL  AFTER `ad_slot_id`;
 ALTER TABLE `ad_slot_stat` ADD `publisher_name` VARCHAR(150)  NULL  DEFAULT NULL  AFTER `publisher_id`;
 ALTER TABLE `ad_slot_stat` ADD `site_name` VARCHAR(100)  NULL  DEFAULT NULL  COMMENT '站点/app 名称'  AFTER `site_id`;
+ALTER TABLE `site_ad_placement` ADD INDEX `uniq_idx_sadp_code` (`code`);
