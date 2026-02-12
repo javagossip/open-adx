1149718ff565214cbf291e0859fbcd6cd9fa9e98
javagossip <javagossip@gmail.com>
2026-02-13 01:10:32 +0800

    feat: 修改日志采样配置服务以使用元数据缓存服务

--- open-adexchange-api/open-adexchange-ssp-api/src/main/java/top/openadexchange/openapi/ssp/application/service/LogSamplingConfigService.java
+++ open-adexchange-api/open-adexchange-ssp-api/src/main/java/top/openadexchange/openapi/ssp/application/service/LogSamplingConfigService.java
@@ -6,6 +6,7 @@ import jakarta.annotation.Resource;
 import top.openadexchange.oax.model.proto.OaxModelsProto.LogSamplingConfig;
 import top.openadexchange.oax.model.proto.OaxModelsProto.LogType;
 import top.openadexchange.openapi.ssp.domain.gateway.IndexService;
+import top.openadexchange.openapi.ssp.domain.gateway.MetadataCacheService;
 import top.openadexchange.openapi.ssp.domain.gateway.OaxEngineServices;
 
 @Service
@@ -13,11 +14,17 @@ public class LogSamplingConfigService {
 
     @Resource
     private OaxEngineServices oaxEngineServices;
+    @Resource
+    private MetadataCacheService metadataCacheService;
 
     public int getLogSamplingRate(LogType logType, Integer mediaId, Integer adSlotId, Integer dspId) {
         IndexService indexService = oaxEngineServices.getIndexService();
-        LogSamplingConfig lsc = indexService.getLsc(logType, mediaId, adSlotId, dspId);
+        Integer lscId = indexService.getLscId(logType, mediaId, adSlotId, dspId);
 
+        if (lscId == null) {
+            return 0;
+        }
+        LogSamplingConfig lsc = metadataCacheService.getLogSamplingConfig(lscId);
         return lsc.getSamplingRate();
     }
 }
